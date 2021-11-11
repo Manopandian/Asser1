a92a84310862fca2134f240f806254409b10f4bf
Manopandian <vr46mano@gmail.com>
2021-11-11 23:56:20 +0530

    messgage

---
+++ src/org/base/ArList.java
@@ -0,0 +1,21 @@
+package org.base;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArList {
+	public static void main(String[] args) {
+		List ex = new ArrayList();
+		ex.add(10);
+		ex.add(10000001);
+		ex.add(10.12f);
+		ex.add("haii");
+		ex.add("A");
+		ex.add(true);
+		System.out.println(ex);
+		
+	}
+	
+	
+
+}
